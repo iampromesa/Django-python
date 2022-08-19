7717c6007c413a1771ffced72dda416180737342
Promise <69802374+promise007@users.noreply.github.com>
2022-08-19 04:30:49 +0100

    commit

---
+++ Python.py
@@ -0,0 +1,6 @@
+price = 10
+rating = 4.9
+name = "mosh"
+is_published = True
+print(price)
+print(rating)
---
+++ var.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+print("My name is", name ,"i am",age)
+
